62248ef23cc750eadb760e0603ea262771681e59
Christopher Quach <quachcw@uci.edu>
2022-01-02 21:29:19 -0800

    finish landing page

--- src/Landing.js
+++ src/Landing.js
@@ -6,6 +6,7 @@ import jennyPic from './images/pic-of-me.gif';
 import { Container } from "react-bootstrap";
 import { Row } from "react-bootstrap";
 import { Col } from "react-bootstrap";
+
 function Landing() {
   return (
     <Container style={{maxWidth:"1100px",paddingTop:"5em"}}>
@@ -22,6 +23,12 @@ function Landing() {
               <Row className="intro">
               <div>I’m a fourth year <strong>mechanical engineering</strong> student with a <strong>design minor</strong> at <strong>uc san diego</strong>. I plan on graduating in june 2022! In the meantime, I’m looking for product design & mechanical engineering jobs as a new grad. Feel free to take a look around my website and connect!</div>  
               </Row>
+              <Col className="d-flex align-items-center justify-content-center" id="socials">
+                <Row >
+                  <Col className='linkedin'>linkedin</Col>
+                  <Col className='email'>email</Col>
+                </Row>
+              </Col>
             </Col>
           </Row>
     </Container>
